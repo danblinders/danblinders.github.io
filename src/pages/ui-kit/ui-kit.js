9b0c856dc8489cb2aab210608eeb25c8d7e4217a
DenisBlinder <denisblinder2018@gmail.com>
2021-08-10 23:38:35 +1000

    Modified pagination component, deleted pagination.js plugin

--- src/pages/ui-kit/ui-kit.js
+++ src/pages/ui-kit/ui-kit.js
@@ -2,7 +2,7 @@ import setLikeButton from "../../templates/components/like-button/like-button";
 import expandList from "../../templates/components/expandable-checkbox-list/expandable-checkbox-list";
 import setRateButton from "../../templates/components/rate-button/rate-button";
 import rangeSlider from "../../templates/components/range-slider/range-slider";
-import setPagination from "../../templates/components/pagination/pagintaion";
+import Pagination from "../../templates/components/pagination/pagintaion";
 import "./ui-kit.scss";
 
 $(document).ready(() => {
@@ -50,9 +50,12 @@ $(document).ready(() => {
     });
   });
 
-  $(".pagination").each(function() {
-    setPagination({
-      paginationSelector: this
-    });
+  $(".pagination-container").each(function() {
+    new Pagination({
+      parent: this,
+      totalPages: 15,
+      activePage: 2,
+      edges: 3
+    }).render();
   });
 });
--- src/templates/components/pagination/pagintaion.js
+++ src/templates/components/pagination/pagintaion.js
@@ -1,11 +1,53 @@
-import * as pagination from "paginationjs";
+export default class Pagination {
+  constructor({parent, totalPages, activePage = 1, edges}) {
+    this.parent = $(parent);
+    this.totalPages = totalPages;
+    this.activePage = activePage;
+    this.edges = edges;
+  }
 
-const setPagination = ({paginationSelector} = {}) => {
-  const paginationContainer = $(paginationSelector);
+  render(page = this.activePage) {
 
-  paginationContainer.pagination({
-    dataSource: [1, 2, 3, 4, 5]
-  });
-};
+    let paginationContent = "",
+        paginationPages = "",
+        beforePage = page - 1,
+        afterPage = page + 1;
 
-export default setPagination;
+    if (page > 1) {
+      paginationContent += "<div class='pagination__prev'>arrow_back</div>";
+    }
+
+    if (page > 2) {
+      paginationPages += "<li class='pagination__page'>1</li>";
+
+      if (page > this.edges) {
+        paginationPages += "<li class='pagination__dots'>...</li>";
+      }
+    }
+
+    for(let pageItem = beforePage; pageItem <= afterPage; pageItem++) {
+
+      if (pageItem === 0 || pageItem === this.totalPages + 1) {
+        continue;
+      } else {
+        paginationPages += `<li class="pagination__page">${pageItem}</li>`;
+      }
+    }
+
+    if (page < this.totalPages - 1) {
+      if (page < this.totalPages - this.edges) {
+        paginationPages += "<li class='pagination__dots'>...</li>";
+      }
+
+      paginationPages += `<li class="pagination__page">${this.totalPages}</li>`;
+    }
+
+    paginationContent += `<ul class='pagination__pages'>${paginationPages}</ul>`;
+
+    if (page < this.totalPages) {
+      paginationContent += "<div class='pagination__next'>arrow_forward</div>";
+    }
+
+    this.parent.html(`<div class="pagination">${paginationContent}</div>`);
+  }
+}
