cf67efd42fd1182d996019150d1aa85e01aa9aee
DenisBlinder <denisblinder2018@gmail.com>
2021-08-30 20:29:17 +1000

    Modified date-dropdown and text-field components

--- src/templates/components/datepicker-block/datepicker-block.js
+++ src/templates/components/datepicker-block/datepicker-block.js
@@ -6,19 +6,33 @@ export default class Datepicker {
     this.datepickerType = this.container.attr("data-datepicker-type");
     this.dateInitialStart = this.container.attr("data-initial-start") ? this.container.attr("data-initial-start") : null;
     this.dateInitialEnd = this.container.attr("data-initial-end") ? this.container.attr("data-initial-end") : null;
+
+    if (this.datepickerType === "multiple-fields") {
+      this.datepickerStartField = this.container.find(".datepicker-block__field[data-input='start']");
+      this.datepickerEndField = this.container.find(".datepicker-block__field[data-input='end']");
+
+      this.datepickerStartFieldInput = this.container.find(".datepicker-block__field[data-input='start'] .text-field__input");
+      this.datepickerEndFieldInput = this.container.find(".datepicker-block__field[data-input='end'] .text-field__input");
+    } else {
+      this.datepickerRangeField = this.container.find(".datepicker-block__field");
+      this.datepickerRangeFieldInput = this.container.find(".datepicker-block__field .text-field__input");
+    }
+
     this.serviceField = this.container.find(".datepicker-block__service-field .text-field__input");
     this.currentDate = currentDate;
     this.startCalendarDate = startCalendarDate;
+
+    this.applyChanges(new Date(this.dateInitialStart), new Date(this.dateInitialEnd));
   }
 
   addButtons() {
     return `<div class='datepicker--buttons'>
-              <div class='datepicker--button' data-action='clear'>
+              <div class='datepicker--main-button' data-action='clear'>
                 <button class='main-button main-button_simple'>
                   <span class="main-button__inner">очистить</span>
                 </button>
               </div> 
-              <div class='datepicker--button' data-action='apply'>
+              <div class='datepicker--main-button' data-action='apply'>
                 <button class='main-button main-button_simple'>
                   <span class="main-button__inner">применить</span>
                 </button>
@@ -26,31 +40,48 @@ export default class Datepicker {
             </div>`;
   }
 
+  transformDate(unformattedDate){
+    let formattedDate;
+
+    if (this.datepickerType === "multiple-fields") {
+      formattedDate = `${unformattedDate.toLocaleString("ru-ru", {dateStyle: "short"})}`;
+    } else {
+      formattedDate = unformattedDate.toLocaleString("ru-ru", {day: "numeric", month: "short"}).replace(/\./, "");
+    }
+
+    return formattedDate;
+  }
+
+  applyChanges(date1, date2) {
+    if (date1 !== undefined && date2 !== undefined) {
+      if (this.datepickerType === "multiple-fields") { 
+        this.datepickerStartFieldInput.val(this.transformDate(date1));
+        this.datepickerEndFieldInput.val(this.transformDate(date2));
+      } else {
+        this.datepickerRangeFieldInput.val(`${this.transformDate(date1)} - ${this.transformDate(date2)}`);
+      }
+    } else {
+      console.error("Choose both dates");
+    }
+  }
+
+  clearDatepicker(datepickerElement) {
+    datepickerElement.clear();
+
+    if (this.datepickerType === "multiple-fields") { 
+      this.datepickerStartFieldInput.val("");
+      this.datepickerEndFieldInput.val("");
+    } else {
+      this.datepickerRangeFieldInput.val("");
+    }
+  }
+
+
+
   render() {
-
-    const datepickerInputStart = this.container.find(".datepicker-block__field[data-input='start'] .text-field__input"),
-          datepickerInputEnd = this.container.find(".datepicker-block__field[data-input='end'] .text-field__input");
-
-    console.log(this.dateInitialEnd);
-    const transformDate = (unformattedDate) => {
-      const day = (unformattedDate.getDate() < 10 ? "0" : "" ) + unformattedDate.getDate(),
-                month = (unformattedDate.getMonth() + 1  < 10 ? "0" : "") + (unformattedDate.getMonth() + 1),
-                year = unformattedDate.getFullYear();
-            
-      const formattedDate = `${day}.${month}.${year}`;
-  
-      return formattedDate;
-    };
-
-    const clearDatepicker = (datepickerElement, inputStart, inputEnd) => {
-      datepickerElement.clear();
-      
-      inputStart.val("");
-      inputEnd.val("");
-    };
-
     const datepickerElem = this.serviceField.datepicker({
       language: "ru",
+      offset: 5.5,
       range: true,
       currentDate: this.currentDate,
       startDate: this.startCalendarDate,
@@ -70,41 +101,35 @@ export default class Datepicker {
             classes: "-current-"
           };
         }
-      },
-      onSelect: function(formattedDate, date) {
-        if (datepickerElem.selectedDates[0]) {
-          const transformedDateStart = transformDate(datepickerElem.selectedDates[0]);
-
-          datepickerInputStart.val(transformedDateStart);
-          datepickerInputStart.attr("data-start",transformedDateStart);
-        }
-        
-        if (datepickerElem.selectedDates[1]) {
-          const transformedDateEnd = transformDate(datepickerElem.selectedDates[1]);
-          
-          datepickerInputEnd.val(transformedDateEnd);
-          datepickerInputEnd.attr("data-start", transformedDateEnd);
-        }
       }
     }).data("datepicker");
-
-
-    datepickerInputStart.on("click", () => {
-      datepickerElem.show();
-    });
-
-    datepickerInputEnd.on("click", () => {
-      datepickerElem.show();
-    });
-
-    datepickerElem.selectDate([new Date(this.dateInitialStart), new Date(this.dateInitialEnd)]);
 
     const calendar = datepickerElem.$datepicker;
 
     calendar.append(this.addButtons());
 
-    calendar.find(".datepicker--button[data-action='clear']").on("click", () => {
-      clearDatepicker(datepickerElem, datepickerInputStart, datepickerInputEnd);
+    calendar.find(".datepicker--btn[data-action='clear']").on("click", () => {
+      this.clearDatepicker(datepickerElem);
     });
+
+    calendar.find(".datepicker--btn[data-action='apply']").on("click", () => {
+      this.applyChanges(datepickerElem.selectedDates[0], datepickerElem.selectedDates[1]);
+      datepickerElem.hide();
+    });
+
+    if (this.datepickerType === "multiple-fields") { 
+      this.datepickerStartField.on("click", () => {
+        datepickerElem.show();
+      });
+      this.datepickerEndField.on("click", () => {
+        datepickerElem.show();
+      });
+    } else {
+      this.datepickerRangeField.on("click", () => {
+        datepickerElem.show();
+      });
+    }
+
+    datepickerElem.selectDate([new Date(this.dateInitialStart), new Date(this.dateInitialEnd)]);
   }
 }
