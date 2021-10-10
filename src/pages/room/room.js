78385cea1845d676b7a46a5a852df9fa8b5c2384
DenisBlinder <denisblinder2018@gmail.com>
2021-10-10 23:05:51 +1000

    Modified donut-chart component and created room page

---
+++ src/pages/room/room.js
@@ -0,0 +1,26 @@
+import inputMask from "../../templates/components/text-field/text-field";
+import Dropdown from "../../templates/components/dropdown/dropdown";
+import Calendar from "../../templates/components/calendar/calendar";
+import {DatepickerWithMultipleFields, DatepickerWithSingleField} from "../../templates/components/datepicker-block/datepicker-block";
+import Header from "../../templates/components/header/header";
+import "./room.scss";
+
+$(document).ready(() => {
+  inputMask();
+  
+  $(".dropdown").each(function() {
+    const dropdownInstance = new Dropdown(this);
+  });
+
+  $(".datepicker-block").each(function() {
+    if ($(this).attr("data-datepicker-type") === "multiple-fields") {
+      new DatepickerWithMultipleFields(this);
+    } else {
+      new DatepickerWithSingleField(this);
+    }
+  });
+
+  $(".header").each(function() {
+    new Header(this);
+  });
+});
